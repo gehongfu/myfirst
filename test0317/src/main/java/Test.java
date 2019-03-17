4c50ae5ea617df80c4808ba0c9ccfce2a9550adf
gehongfu <gehongfu@haima.me>
2019-03-17 20:33:11 +0800

    add file1

--- test0317/src/main/java/Test.java
+++ test0317/src/main/java/Test.java
@@ -2,6 +2,8 @@ public class Test {
 
     public static void main(String [] args)
     {
-        System.out.println("hello");
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
