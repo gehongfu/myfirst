f853044ad968e7078d75618c4ad8066ad6e21cf2
gehongfu <gehongfu@haima.me>
2019-03-17 21:22:51 +0800

    add file1

--- test0317/src/main/java/Test.java
+++ test0317/src/main/java/Test.java
@@ -1,2 +1,13 @@
 public class Test {
+
+    public static void main(String [] args)
+    {
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello3");
+        System.out.println("hello 4");
+        System.out.println("hello 5");
+        System.out.println("hello 6");
+    }
 }
